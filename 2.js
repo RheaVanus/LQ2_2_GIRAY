3f37b52320a69e7632623b5728debfcc4bd8ec41
Jerome Giray <jeromegiray500@gmail.com>
2025-03-26 04:39:30 +0800

    Completed Iteration Section

--- 2.js
+++ 2.js
@@ -5,9 +5,9 @@ Using a LOOP STATEMENT and prompt()
 
 */
 
-let dog_breed = prompt("Enter a dog breed:");
-let numOfIteration = parseInt(prompt("Enter the number of loops:"));
+let dog_breed = prompt("Enter a dog breed:"); //prompts the user the name of the dog breed to be looped.
+let numOfIteration = parseInt(prompt("Enter the number of loops:")); //prompts the user if how many times does he/she wants to repeat the namne
 
-for (let i = 0 ; i < numOfIteration; i++){
-    console.log(dog_breed)
+for (let i = 0 ; i < numOfIteration; i++){ //let iteration eqals to 0. The number of loops depends to the numOfIteration, as long as it is greater than 0. it will loop. i++ for safety loop.
+    console.log(dog_breed)//logged the dog_breed name along with the loop.
 }
